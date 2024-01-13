343f6244b613e6d7a972f53acfee7c0db7030577
Matthew03192 <hllc12@durham.ac.uk>
2024-01-13 16:07:47 +0000

    still trying to get server stuff to work

--- node.js
+++ node.js
@@ -1,5 +1,12 @@
 const express = require('express')
 const app = express()
 app.use(express.static('client'));
+app.use(express.json());
+
+let searchHTML = "<div class='mb-3'><label for='exampleFormControlInput1' class='form-label'>Email address</label><input type='email' class='form-control' id='exampleFormControlInput1' placeholder='name@example.com'></div>;";
+app.get('/search', function (req, resp){
+    resp.send(searchHTML);
+ });
+
 
 app.listen(8090)
